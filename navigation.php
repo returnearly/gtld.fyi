86dcd2b25cc2562d990db5a26a30edcbe10864b7
Tom Schlick <tom@returnearly.net>
2023-08-07 09:18:28 -0400

    remove default docs

--- navigation.php
+++ navigation.php
@@ -4,14 +4,5 @@ return [
     'Home' => '/',
     'About' => '/about',
     'All TLD Extensions' => '/all',
-    'Getting Started' => [
-        'url' => 'docs/getting-started',
-        'children' => [
-            'Customizing Your Site' => 'docs/customizing-your-site',
-            'Navigation' => 'docs/navigation',
-            'Algolia DocSearch' => 'docs/algolia-docsearch',
-            'Custom 404 Page' => 'docs/custom-404-page',
-        ],
-    ],
     'Edit on Github' => 'https://github.com/returnearly/gtld.fyi/',
 ];
